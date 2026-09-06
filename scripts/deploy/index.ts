8e634e1e7655487fe3a69371cb21658129be9716
Maya Thomas <mayathomasmaya@outlook.com>
2026-09-06 20:45:04 +0800

    Update index.ts

--- scripts/deploy/index.ts
+++ scripts/deploy/index.ts
@@ -282,7 +282,8 @@ const pushPagesSecret = () => {
     'AUTH_GITHUB_SECRET', 
     'AUTH_GOOGLE_ID', 
     'AUTH_GOOGLE_SECRET', 
-    'AUTH_SECRET'
+    'AUTH_SECRET',
+    'AUTH_URL'
   ];
 
   try {
