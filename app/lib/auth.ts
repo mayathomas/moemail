c553578611fe82eddecc9378d31d4c8be81a976b
Maya Thomas <mayathomasmaya@outlook.com>
2026-09-06 21:01:04 +0800

    Update auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -93,10 +93,8 @@ export const {
   auth,
   signIn,
   signOut
-} = NextAuth(() => {
-  const env = getRequestContext().env as any;
-  return {
-  secret: env.AUTH_SECRET,
+} = NextAuth(() => ({
+  secret: process.env.AUTH_SECRET,
   trustHost: true,
   adapter: DrizzleAdapter(createDb(), {
     usersTable: users,
@@ -104,13 +102,13 @@ export const {
   }),
   providers: [
     GitHub({
-      clientId: env.AUTH_GITHUB_ID,
-      clientSecret: env.AUTH_GITHUB_SECRET,
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
       allowDangerousEmailAccountLinking: true,
     }),
     Google({
-      clientId: env.AUTH_GOOGLE_ID,
-      clientSecret: env.AUTH_GOOGLE_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
       allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
